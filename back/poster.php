303ff6123f0363352f4ea35cd93e47994ad99591
DuskEVE <x987000@gmail.com>
2024-01-11 14:13:52 +0800

    finish poster edit function in admin

--- back/poster.php
+++ back/poster.php
@@ -24,40 +24,46 @@
     <div class="ct" style="width: 24%; margin:5px 0 5px 0; background-color: gray">操作</div>
 </div>
 
-<div style="width: 100%; height: 190px; overflow: auto">
-    <?php
-    $datas = $Poster->searchAll();
-    foreach($datas as $index=>$data){
-    ?>
-    <div class="item">
-        <div>
-            <img src="./img/<?=$data['img']?>" style="height: 90px;">
+<form action="./api/edit_poster.php" method="post">
+    <div style="width: 100%; height: 190px; overflow: auto">
+
+        <?php
+        $datas = $Poster->searchAll();
+        foreach($datas as $index=>$data){
+        ?>
+        <div class="item">
+            <div>
+                <img src="./img/<?=$data['img']?>" style="height: 90px;">
+            </div>
+            <div>
+                <input type="text" name="name[]" value="<?=$data['name']?>">
+            </div>
+            <div>
+                <input type="button" value="往上">
+                <input type="button" value="往下">
+            </div>
+            <div>
+                <input type="checkbox" name="display[]" value="<?=$data['id']?>" <?=($data['display']==1? "checked":"")?>>顯示
+                <input type="checkbox" name="del[]" value="<?=$data['id']?>">刪除
+                <select name="ani[]">
+                    <option value="1" <?=($data['ani']==1?"selected":"")?>>淡入淡出</option>
+                    <option value="2" <?=($data['ani']==2?"selected":"")?>>收縮</option>
+                    <option value="3" <?=($data['ani']==3?"selected":"")?>>滑入滑出</option>
+                </select>
+
+                <input type="hidden" name="id[]" value="<?=$data['id']?>">
+            </div>
         </div>
-        <div>
-            <input type="text" name="name[]" value="<?=$data['name']?>">
-        </div>
-        <div>
-            <input type="button" value="往上">
-            <input type="button" value="往下">
-        </div>
-        <div>
-            <input type="checkbox" name="display[]" value="<?=$data['id']?>" <?=($data['display']==1? "checked":"")?>>顯示
-            <input type="checkbox" name="del[]" value="<?=$data['id']?>">刪除
-            <select name="ani">
-                <option value="1">淡入淡出</option>
-                <option value="2">收縮</option>
-                <option value="3">滑入滑出</option>
-            </select>
-        </div>
+        <?php
+        }
+        ?>
     </div>
-    <?php
-    }
-    ?>
-</div>
 
-<div>
-
-</div>
+    <div class="ct">
+        <input type="submit" value="編輯確定">
+        <input type="reset" value="重置">
+    </div>
+</form>
 
 <hr>
 <div>
